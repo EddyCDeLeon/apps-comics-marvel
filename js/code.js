c6d9d6b318a2280552311673bacef697d2fb6a32
Isaak Ordonez <isaak@zeruen.com>
2015-11-10 16:06:03 -0600

    lista de categorias de comit con witch case, se agregaron las categorias de marvel como array

--- js/code.js
+++ js/code.js
@@ -5,54 +5,55 @@ angular.module('ComicApp', ['angular-md5'])
         vm.mensajewelcome = "Bienvenidos";
         vm.mensajeComic = "Los mejores comics de Super Heroes de Marvel";
         vm.mensajeSearch = "Buscar Comics por:";
+        vm.datosCategoria = [];
         vm.list = function() {
             return MarvelService.list();
         };
         vm.ButtonClick = function () {
-
-            console.log(vm.repeatSelect);
-            console.log(MarvelService.listComics());
+            //console.log(vm.repeatSelect);
+            datosCategoria = MarvelService.listComics(vm.repeatSelect);
+            console.log(datosCategoria);
 
         };
-        var init = function() {
+        /*var init = function() {
             MarvelService.getComics();
         };
-        init();
+        init();*/
 
     }])
     .service('MarvelService', ['$http', 'md5', function($http, md5) {
         var categories = [{
-            title: 'Series',
+            title: 'Characteres',
             img: 'portrait_incredible',
             nombre: 'Nombre de Ejemplo 1',
             id: '1',
             autor: 'Carlos olmedo'
         }, {
-            title: 'Ediciones',
+            title: 'Comics',
             img: 'portrait_incredible2',
             nombre: 'Nombre de Ejemplo 2',
             id: '2',
             autor: 'Eddy Leon'
         }, {
-            title: 'Superpoderes',
+            title: 'Creator',
             img: 'portrait_incredible3',
             nombre: 'Nombre de Ejemplo 3',
             id: '3',
             autor: 'Katarina Solar'
         }, {
-            title: 'Historias',
+            title: 'Events',
             img: 'portrait_incredible4',
             nombre: 'Nombre de Ejemplo 4',
             id: '4',
             autor: 'Melvin Castro'
         }, {
-            title: 'Villanos',
+            title: 'Series',
             img: 'portrait_incredible5',
             nombre: 'Nombre de Ejemplo 5',
             id: '5',
             autor: 'Sonia Jax'
         }, {
-            title: 'Origenes',
+            title: 'Stories',
             img: 'portrait_incredible6',
             nombre: 'Nombre de Ejemplo 6',
             id: '6',
@@ -64,11 +65,29 @@ angular.module('ComicApp', ['angular-md5'])
         this.add = function(category) {
             categories.push(category);
         };
-        this.listComics = function () {
+        this.listComics = function (selection) {
           return comics;
+
+          switch(selection) {
+              case n:
+                  code block
+                  break;
+              case n:
+                  code block
+                  break;
+              default:
+                  console.log("En listComics", selection);
+          }
+
         };
 
-        var comics = [];
+        var characteresList = [];
+        var comicsList  = [];
+        var creatorList  = [];
+        var eventsList  = [];
+        var seriesList  = [];
+        var storiesList  = [];
+
         var publicKey = 'a4703ddc654f493c1b7923a859f68e4a';
         var privateKey = '6fed95bf3af7a38b3405f2c50e65d380b9dd2758';
         var baseUrl = 'http://gateway.marvel.com/v1/';
